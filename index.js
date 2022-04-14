aa9df2585ee9e3b89712d3aadb4222a4f3e7bd34
Tapan <educationtalk2021@gmail.com>
2022-04-14 20:03:42 +0530

    Changed Somethinggit status!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 console.log("Welcome to NodeJs");
 console.log("NodeJs is Server Side Java Script");
 console.log("NodeJs can be used for the CRUD Operation");
+console.log("Canged The Contents ......");
 var x=30;
 var y=20;
 console.log(x+y);
