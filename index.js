41cf20c23fea6dfb771c62c94d306b1f45e4574a
Tapan <educationtalk2021@gmail.com>
2022-04-14 20:26:52 +0530

    added package! 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("Welcome to NodeJs");
 console.log("NodeJs is Server Side Java Script");
 console.log("NodeJs can be used for the CRUD Operation");
 console.log("Canged The Contents ......");
+console.log("Canged The Contents ......");
 var x=30;
 var y=20;
 console.log(x+y);
