d1c417470be1dc0062576ec22090924e0e5aa9c2
Tapan <educationtalk2021@gmail.com>
2022-04-14 19:52:50 +0530

    NodeJs 1st Task Exaample

---
+++ index.js
@@ -0,0 +1,8 @@
+
+console.log("Welcome to NodeJs");
+console.log("NodeJs is Server Side Java Script");
+console.log("NodeJs can be used for the CRUD Operation");
+var x=30;
+var y=20;
+console.log(x+y);
+
